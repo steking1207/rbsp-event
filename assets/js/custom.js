7f74ddd3d89d6ee879779bba70986ea8b2f28d26
steking1207 <steking1207@gmail.com>
2019-09-30 10:19:36 +0800

    190930

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,4 +4,8 @@ $(function(){
         scrollTop: $( $.attr(this, 'href') ).offset().top - 120}, 750);
 		return false;
 	});
+    $('#customFile').on('change',function(){
+        var fileName = $(this).val();
+        $(this).next('.custom-file-label').html(fileName);
+    });
 });
